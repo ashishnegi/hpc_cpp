3402bd4d46db1380c710333553fd4d0af9256dfa
Ashish Negi <ashishnegi33@gmail.com>
2020-04-12 19:06:43 -0700

    Sleep for some time if we don't get the spinlock.. See CPU time is less atleast.

--- spinlocks.cpp
+++ spinlocks.cpp
@@ -1,6 +1,8 @@
 #include <benchmark/benchmark.h>
 #include <mutex>
 #include <atomic>
+#include <chrono>
+#include <thread>
 
 namespace spinlocks
 {
@@ -10,7 +12,20 @@ namespace spinlocks
         Spinlock() { flag.clear(); }
 
         void lock() {
-            while (flag.test_and_set()) {}
+            while (true) {
+                int tryLefts = 10;
+                while (tryLefts > 0) {
+                    if (!flag.test_and_set()) {
+                        return;
+                    }
+                    tryLefts -= 1;
+                }
+
+                // this is better than no sleeping
+                // but it sleeps for more than 1 nanoseconds..
+                // so it does not beat the mutex.
+                std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+            }
         }
 
         void unlock() {
@@ -21,17 +36,19 @@ namespace spinlocks
         std::atomic_flag flag;
     };
 
-    long *px = new long();
-    long &x = *px;
     Spinlock spinlock;
+    volatile long dummy_x;
+    volatile long *px = new long();
+    volatile long &x = *px;
 
-    static void BM_noSpinLockAdd(benchmark::State& state) {
+    static void BM_noSpinlockAdd(benchmark::State& state) {
         if (state.thread_index == 0) {
             x = 0;
         }
 
         while (state.KeepRunning()) {
-            benchmark::DoNotOptimize(x++);
+            x += 1;
+            benchmark::DoNotOptimize(x);
         }
 
         benchmark::ClobberMemory();
@@ -44,7 +61,8 @@ namespace spinlocks
 
         while (state.KeepRunning()) {
             std::lock_guard<Spinlock> locked(spinlock);
-            benchmark::DoNotOptimize(x++);
+            x += 1;
+            benchmark::DoNotOptimize(x);
         }
 
         benchmark::ClobberMemory();
@@ -54,12 +72,11 @@ namespace spinlocks
     #define ARGS(N) ->Threads(N)->UseRealTime();
 
     // Register the function as a benchmark
-    BENCHMARK(BM_noSpinLockAdd) ARGS(1);
-    BENCHMARK(BM_noSpinLockAdd) ARGS(4);
-    BENCHMARK(BM_noSpinLockAdd) ARGS(8);
-    BENCHMARK(BM_noSpinLockAdd) ARGS(12);
-    BENCHMARK(BM_noSpinLockAdd) ARGS(16);
+    BENCHMARK(BM_noSpinlockAdd) ARGS(1);
+    BENCHMARK(BM_noSpinlockAdd) ARGS(4);
+    BENCHMARK(BM_noSpinlockAdd) ARGS(8);
     BENCHMARK(BM_SpinlockAdd) ARGS(1);
+    BENCHMARK(BM_SpinlockAdd) ARGS(2);
     BENCHMARK(BM_SpinlockAdd) ARGS(4);
     BENCHMARK(BM_SpinlockAdd) ARGS(8);
     BENCHMARK(BM_SpinlockAdd) ARGS(12);
